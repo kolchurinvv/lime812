1c90936536e38151cadc1555432067c067eb63ef
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2023-01-03 08:44:36 +0100

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-    let counter = 0;
+  let counter = 0
 </script>
-<style>
-    button {
-        background: green;
-        color:#FFF;
-    }
-</style>
 
 I'm counting ... {counter}
-<button on:click={() => {counter++}}>Count up</button>
+<button
+  on:click={() => {
+    counter++
+  }}>Count up</button
+>
+
+<style>
+  button {
+    background: green;
+    color: #fff;
+  }
+</style>
