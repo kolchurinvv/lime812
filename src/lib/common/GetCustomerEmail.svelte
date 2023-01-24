6202caf02e5fd809378a82e704cf49d16dcebbeb
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2023-01-24 21:29:22 +0100

    email service working

--- src/lib/common/GetCustomerEmail.svelte
+++ src/lib/common/GetCustomerEmail.svelte
@@ -1,11 +1,38 @@
 <script lang="ts">
+  import { onMount } from "svelte"
+  import emailjs from "@emailjs/browser"
   export let placeholderText: string = "Ваша почта"
   export let key: string
+  let sendersEmail: string = ""
+  const logKey = (e: KeyboardEvent) => {
+    if (e.code === "Enter") {
+      emailjs
+        .send(
+          "service_49mn7mg",
+          "template_eqvxrrl",
+          {
+            from_name: sendersEmail,
+            to_name: "Elena",
+            message: "i wanna order stuff",
+            reply_to: sendersEmail,
+          },
+          "FY8zGK7A9e9L2XImB"
+        )
+        .then(
+          (response) => {
+            console.log("SUCCESS!", response.status, response.text)
+          },
+          (err) => {
+            console.log("FAILED...", err)
+          }
+        )
+    }
+  }
 </script>
 
 <div class="field label prefix border ">
   <i>email</i>
-  <input type="email" id={key} />
+  <input type="email" on:keydown={logKey} bind:value={sendersEmail} id={key} />
   <label class="secondary transparent" for={key}>{placeholderText}</label>
 </div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { sveltekit } from "@sveltejs/kit/vite"
-// import type { UserConfig } from "vite"
 import { defineConfig, loadEnv } from "vite"
 import path from "path"
 // TODO create a plugin that hooks into HMR to regenerate the theme when change is detected
@@ -9,16 +8,18 @@ const config = defineConfig(({ mode }) => {
   return {
     envDir: "/",
     plugins: [sveltekit()],
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
+        "@@": path.resolve(__dirname, "./"),
       },
     },
   }
 })
-
-// const config: UserConfig = {
-// plugins: [sveltekit()],
-// }
 
 export default config
