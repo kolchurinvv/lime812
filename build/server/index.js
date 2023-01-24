5b968bffda2322a09911b2f4f386ce28020350da
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2023-01-24 22:11:04 +0100

    another build

--- build/server/index.js
+++ build/server/index.js
@@ -3420,7 +3420,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1674592176393"
+      version: "1674594431529"
     };
   }
   async init({ env }) {
