284a54adb556aa79535958516a9b660ea7cea007
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2023-06-09 12:52:17 +0200

    no more build folder in commits hopefully

--- build/server/chunks/0-dbbc2ee9.js
+++
@@ -1,9 +0,0 @@
-const index = 0;
-const component = async () => (await import('./_layout.svelte-5abfcd90.js')).default;
-const file = '_app/immutable/components/pages/_layout.svelte-ef101f47.js';
-const imports = ["_app/immutable/components/pages/_layout.svelte-ef101f47.js","_app/immutable/chunks/index-4a554cae.js","_app/immutable/chunks/GetCustomerEmail-03c9f3da.js"];
-const stylesheets = ["_app/immutable/assets/_layout-6a0f973c.css","_app/immutable/assets/GetCustomerEmail-5a3cb573.css"];
-const fonts = [];
-
-export { component, file, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-dbbc2ee9.js.map
--- build/server/chunks/2-ca784ec3.js
+++
@@ -1,9 +0,0 @@
-const index = 2;
-const component = async () => (await import('./_page.svelte-52e98967.js')).default;
-const file = '_app/immutable/components/pages/_page.svelte-9601bc1b.js';
-const imports = ["_app/immutable/components/pages/_page.svelte-9601bc1b.js","_app/immutable/chunks/index-4a554cae.js","_app/immutable/chunks/GetCustomerEmail-03c9f3da.js"];
-const stylesheets = ["_app/immutable/assets/_page-c99e8f41.css","_app/immutable/assets/GetCustomerEmail-5a3cb573.css"];
-const fonts = [];
-
-export { component, file, fonts, imports, index, stylesheets };
-//# sourceMappingURL=2-ca784ec3.js.map
